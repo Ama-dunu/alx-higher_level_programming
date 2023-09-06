aa5b8aa13f6d998294c180b5e45b1d245f0804dc
Ama-dunu <annettebdunu90@gmail.com>
2023-09-05 23:45:43 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
-for letter in range(ord('a'), ord('z')+1):
-    if chr(letter) not in 'qe':
-        print(chr(letter), end='')
+#!/usr/bin/python3
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")

15251b3c3220673e48bd0d714da90acb6f4e49cb
Ama-dunu <annettebdunu90@gmail.com>
2023-09-26 06:56:53 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,53 @@
+class Square:
+    """
+    Represents a square with a size attribute.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new square with a given size.
+
+        Args:
+            size (int, optional): The size of the square (default is 0).
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If the size is not an integer.
+            ValueError: If the size is negative.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
